b21828959790405723b0fdbb069c1853aa023262
Jelaleddin <atalykovj@gmail.com>
2021-07-07 03:27:52 +0300

    added _redirects file

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,52 +1,3 @@
-// // import React from "react";
-// // import Input from "./Input";
-
-// // function Dashboard({ movie }) {
-// //   console.log(movie);
-// //   const newMovie = { ...movie };
-// //   const { Poster, Ratings } = movie;
-// //   delete newMovie.Poster;
-// //   delete newMovie.Ratings;
-// //   const generalInfoContent = [];
-// //   for (let key in newMovie) {
-// //     const info = (
-// //       <div key={key}>
-// //         {key}: {newMovie[key]}
-// //       </div>
-// //     );
-// //     generalInfoContent.push(info);
-// //   }
-// //   return (
-// //     <div className="container">
-// //       <img src={Poster} alt="Show poster" height="auto" width="220px" />
-// //       <div className="general-info">{generalInfoContent}</div>
-// //     </div>
-// //   );
-// // }
-
-// // export default Dashboard;
-
-// import React, {useState} from 'react'
-// import Input from './Input'
-
-// function Dashboard() {
-//   const [moviesData, setMovieData] = useState();
-//   const onClick = (movieTitle) => {
-//     console.log(movieTitle);
-//     const modifiedMovieTitle = movieTitle.replace(/ /gi, "+");
-//     getMovie(modifiedMovieTitle)
-//       .then((data) => setMovieData(data))
-//       .catch((err) => console.error(err));
-//   };
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// export default Dashboard
-
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import Input from "./Input";
@@ -63,7 +14,8 @@ function Dashboard() {
       {input && (
         <SuggestionsList input={input} onClickToMovie={onClickToMovie} />
       )}
-      {!input && <Route exact path="/:id" component={SingleMovie} />}
+      {/* {!input && <Route exact path="/:id" component={SingleMovie} />} */}
+      <Route exact path="/:id" component={SingleMovie} />
     </section>
   );
 }
